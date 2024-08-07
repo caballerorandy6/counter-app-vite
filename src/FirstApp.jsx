2b41bdce69582574456622ff29d7ee712a53e37f
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2024-08-07 01:00:15 -0500

    Hasta la clase 'Impresion de variables en el HTML'

---
+++ src/FirstApp.jsx
@@ -0,0 +1,14 @@
+const newMessage = { message: "Hello World!", title: "Randy" };
+
+// const getResult = (a, b) => a + b;
+
+export const FirstApp = () => {
+  return (
+    <>
+      {/* <h1>{getResult(1, 2)}</h1> */}
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
+      <h1>Randy</h1>
+      <p>I am a subtitle</p>
+    </>
+  );
+};
