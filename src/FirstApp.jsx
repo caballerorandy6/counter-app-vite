b16b0003a769915a5962831fd13fe6d8053d7b74
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2024-08-09 00:28:58 -0500

    Fin de la seccion 4

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,14 +1,31 @@
-const newMessage = { message: "Hello World!", title: "Randy" };
+import PropTypes from "prop-types"; //Esto se debe de instalar con "npm install prop-types"
 
+//const newMessage = { message: "Hello World!", title: "Randy" };
 // const getResult = (a, b) => a + b;
 
-export const FirstApp = () => {
+export const FirstApp = ({ title, subtitle, name }) => {
+  //console.log(title, subtitle);
+
   return (
     <>
       {/* <h1>{getResult(1, 2)}</h1> */}
       {/* <code>{JSON.stringify(newMessage)}</code> */}
-      <h1>Randy</h1>
-      <p>I am a subtitle</p>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+      <p>{name}</p>
     </>
   );
+};
+
+//Tipado de las props
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+};
+
+//Valores por defecto de las props
+FirstApp.defaultProps = {
+  title: "No hay título",
+  subtitle: "No hay subtítulo",
+  name: "Randy",
 };
